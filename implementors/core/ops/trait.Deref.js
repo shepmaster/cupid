e1ade1f18d97d7018781f49172b7b3e49e7c20df
Travis-CI <builder@travis>
2016-11-20 01:03:44 +0000

    Deployed to Github Pages

---
+++ implementors/core/ops/trait.Deref.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["cupid"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='cupid/struct.BrandString.html' title='cupid::BrandString'>BrandString</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
